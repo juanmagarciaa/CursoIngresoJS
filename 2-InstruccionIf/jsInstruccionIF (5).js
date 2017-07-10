3847a5a5d8d602b7866cc80f6632babee978686d
juanmagarciaa <juanma_garciaa@hotmail.com>
2017-07-09 23:27:41 -0300

    actualización ejercicios entrada y salida / if else

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -2,5 +2,21 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
+
+var mensaje;
+
+mensaje = "usted no es adolescente";
+
+edad = document.getElementById ("edad").value;
+
+if (edad <= 12 || edad >= 18 )
+
+{
+
+alert (mensaje);
+
+}
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,30 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
 
+var MayorEdad;
 
+var MenorEdad;
+
+var Adolescente;
+
+MayorEdad = "usted es mayor de edad";
+
+MenorEdad = "usted es menor de edad";
+
+Adolescente = "usted es adolescente";
+
+edad = document.getElementById("edad").value;
+
+if (edad <= 18)
+
+{
+
+alert (MayorEdad);
+
+}
+
+else ()
 
 }//FIN DE LA FUNCIÓN
