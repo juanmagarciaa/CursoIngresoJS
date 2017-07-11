a7e28e195c4661d109f1afd9e301ff2dac2ca4b3
juanmagarciaa <juanma_garciaa@hotmail.com>
2017-07-11 20:15:49 -0300

    actualizacion de ejercicios while

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -2,5 +2,21 @@ function Mostrar()
 {
 	alert('iteración while');
 
+var num;
+
+num=0;
+
+document.getElementById ("FormIngreso").value = num;
+
+while (num <=9);
+
+{
+
+num= num+1;
+
+alert (num);
+
+}
+
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,21 @@
 function Mostrar()
 {
-	alert('iteración while');
+var num;
+
+num=10;
+
+document.getElementById ("FormIngreso").value;
+
+while (num >= 1);
+
+{
+
+alert (num);
+
+num= num-1;
+
+}
+
 
 
 }//FIN DE LA FUNCIÓN
